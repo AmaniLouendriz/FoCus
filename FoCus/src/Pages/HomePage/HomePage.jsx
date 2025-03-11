66ab2380c036709e5d620f4ca2df498ba53b61d7
Amani <alouendr@uottawa.ca>
2025-03-11 17:33:28 -0400

    started summary page

--- FoCus/src/Pages/HomePage/HomePage.jsx
+++ FoCus/src/Pages/HomePage/HomePage.jsx
@@ -1,3 +1,25 @@
+import './HomePage.css'
+
 export const HomePage = ()=>{
-    return (<>welcome to the home page</>)
+    return (
+        <div className="homeDiv">
+          <h4 className='greetingUser'>Hello User 1</h4>
+          <h5 className='titlePage'>Summary of Activities</h5>
+          
+
+
+
+
+
+
+        </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    )
 }
